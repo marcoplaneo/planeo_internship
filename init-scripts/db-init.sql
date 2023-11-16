b2f279b23d6fcb9efcdb63f037787554f43b97d8
marco <marco.gaus@planeo.de>
2023-11-16 13:53:02 +0100

    added command to create db row 'avatarpath' for avatar pictures

--- init-scripts/db-init.sql
+++ init-scripts/db-init.sql
@@ -3,5 +3,6 @@ CREATE TABLE `internship`.`user` (
     `firstname` TEXT ,
     `username` VARCHAR(50) NOT NULL ,
     `password` CHAR(255) NOT NULL ,
+    `avatarpath` TEXT ,
     PRIMARY KEY (`uid`), UNIQUE (`username`))
     ENGINE = InnoDB;
