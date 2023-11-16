8dc222c3e7437303171d50e8a5c896d232455901
marco <marco.gaus@planeo.de>
2023-11-16 14:14:31 +0100

    when username is changed, the filepath changes too

--- html/profile.php
+++ html/profile.php
@@ -1,49 +1,120 @@
 <?php
-$target_dir = "uploads/";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-$uploadOk = 1;
-$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+session_start();
+include("db.php");
+$username = $_SESSION["username"];
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Profile</title>
+    <link href="css/style.css" rel="stylesheet">
+    <link rel="icon" type="image/x-icon" href="/images/1176favicon.ico">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300&display=swap" rel="stylesheet">
+    <link rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0"/>
+</head>
+<body>
+<!--heading-->
+<h5>Settings</h5>
+<!-- Trigger/Open The Modal -->
+<form method="post">
+    <button type="button" id="changename">Change name/username</button>
+    <br>
 
-// Check if image file is a actual image or fake image
-if(isset($_POST["submit"])) {
-    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-    if($check !== false) {
-        echo "File is an image - " . $check["mime"] . ".";
-        $uploadOk = 1;
-    } else {
-        echo "File is not an image.";
-        $uploadOk = 0;
+    <!-- The Modal -->
+    <div id="changenamemodal" class="modal">
+
+        <!-- Modal content -->
+        <div class="modal-content">
+            <span class="closechangename">&times;</span>
+            <h5>Change name and/or username</h5>
+            <label>Change firstname to<br>
+                <input type="text" id="changefirstname" name="changefirstname"></label><br><br>
+            <label>Change username to<br>
+                <input type="text" id="changeusername" name="changeusername"></label><br><br>
+            <input type="submit" id="applychanges" name="applychanges" value="Apply changes">
+        </div>
+
+    </div>
+
+    <script>
+        // Get the modal
+        var changenamemodal = document.getElementById("changenamemodal");
+
+        // Get the button that opens the modal
+        var changename = document.getElementById("changename");
+
+        // Get the <span> element that closes the modal
+        var spanchangename = document.getElementsByClassName("closechangename")[0];
+
+        // When the user clicks the button, open the modal
+        changename.onclick = function () {
+            changenamemodal.style.display = "block";
+        }
+
+        // When the user clicks on <span> (x), close the modal
+        spanchangename.onclick = function () {
+            changenamemodal.style.display = "none";
+        }
+
+        // When the user clicks anywhere outside of the modal, close it
+        window.onclick = function (event) {
+            if (event.target == changenamemodal) {
+                changenamemodal.style.display = "none";
+            }
+        }
+    </script>
+    <input type="submit" name="deleteuser" value="Delete user">
+</form>
+<?php
+if (isset($_POST["applychanges"])) {
+    if (!empty($_POST["changefirstname"]) && !empty($_POST["changeusername"])) {
+        //change firstname and username
+        $newusername = $_POST["changeusername"];
+        $newfirstname = $_POST["changefirstname"];
+        $sql = "UPDATE user SET username = '$newusername', firstname = '$newfirstname', avatarpath = './images/users/$newusername' WHERE username = '$username'";
+        try {
+            mysqli_query($con, $sql);
+            $_SESSION["name"] = "$newfirstname";
+            $_SESSION["username"] = "$newusername";
+        } catch (mysqli_sql_exception $ex1) {
+            echo "That username is already taken!";
+        }
+    } elseif (!empty($_POST["changefirstname"])) {
+        //change firstname
+        $newfirstname = $_POST["changefirstname"];
+        $sql = "UPDATE user SET firstname = '$newfirstname' WHERE username = '$username'";
+        try {
+            mysqli_query($con, $sql);
+            $_SESSION["name"] = "$newfirstname";
+        } catch (mysqli_sql_exception $ex2) {
+            echo "Oops, something went wrong!";
+        }
+    } elseif (!empty($_POST["changeusername"])) {
+        //change username
+        $newusername = $_POST["changeusername"];
+        $sql = "UPDATE user SET username = '$newusername', avatarpath = './images/users/$newusername' WHERE username = '$username'";
+        try {
+            mysqli_query($con, $sql);
+            $_SESSION["username"] = "$newusername";
+        } catch (mysqli_sql_exception $ex3) {
+            echo "That username is already taken!";
+        }
     }
 }
+if (isset($_POST["deleteuser"])) {
+    $sql = "DELETE FROM user WHERE username = '$username'";
 
-// Check if file already exists
-if (file_exists($target_file)) {
-    echo "Sorry, file already exists.";
-    $uploadOk = 0;
-}
-
-// Check file size
-if ($_FILES["fileToUpload"]["size"] > 500000) {
-    echo "Sorry, your file is too large.";
-    $uploadOk = 0;
-}
-
-// Allow certain file formats
-if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-    && $imageFileType != "gif" ) {
-    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-    $uploadOk = 0;
-}
-
-// Check if $uploadOk is set to 0 by an error
-if ($uploadOk == 0) {
-    echo "Sorry, your file was not uploaded.";
-// if everything is ok, try to upload file
-} else {
-    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-        echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
-    } else {
-        echo "Sorry, there was an error uploading your file.";
-    }
+    mysqli_query($con, $sql);
+    $_SESSION["status"] = "stopped";
+    session_destroy();
+    ?>
+    <script type="text/javascript">
+        window.location.href = 'index.php';
+    </script>
+    <?php
 }
 ?>
+</body>
+</html>
